2a5394cd92bfc12a352fa504897d45e46b97afe7
Erick <simasgrilo86@gmail.com>
2025-03-09 10:54:53 -0300

    Added validation error exception handling when a service logging to the LogAggregator sends an invalid format log

--- LogAggregator.py
+++ LogAggregator.py
@@ -2,6 +2,11 @@ from flask import Flask, request
 from src.Logger.Logger import Logger
 from src.FileTransferManager.FileUploader import FileUploader
 from src.ConfigManager.ConfigManager import ConfigManager
+from src.FileTransferManager.ElasticConnector import ElasticConnector
+from src.FileTransferManager.FileReader import FileReader
+from src.FileTransferManager.FileReaderControl import FileReaderControl
+from pydantic import ValidationError
+import json
 from json import JSONDecodeError
 
 
@@ -22,17 +27,24 @@ class LogAggregator:
     
     def log(self):
         """Method to log messages from different sources. The full structure of the logging messages is defined in class Logger.py
-            this method logs the message using the Logger definition, returning to the client the origin IP of the logged message.s
+            this method logs the message using the Logger definition, returning to the client the origin IP of the logged messages.
+            This endpoint will be used by the clients to send their logs to the aggregator. It will reunite these in files and then send it to Elasticsearch instance.
         Returns:
             None
         """        
-        try:
+        try: 
             self.__log.log(request, request.data)
             return "Log received from {}".format(request.remote_addr), 200  # OK
         except JSONDecodeError as e:
             return "Invalid JSON: {}".format(e.msg), 400
-        except FileNotFoundError and OSError as e:
+        except FileNotFoundError and OSError:
             return "Error writing log to file {}: ".format(e.filename, e.strerror), 500
+        except ValidationError as e:
+            error_msg = {
+                "Error" : "Invalid log entry",
+                "Details" : e.errors()
+            }
+            return json.dumps(error_msg), 500
         
 if __name__ == "__main__":
     log_aggregator = LogAggregator()
