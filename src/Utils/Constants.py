e53688a50c9c15ecfaabeae31592c25d0c9ade0a
Erick <simasgrilo86@gmail.com>
2025-03-14 01:14:07 -0300

    Added constans for HTTP response status codes

---
+++ src/Utils/Constants.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+class Constants(Enum):
+    """
+    Class to maintain constants that will be used throughout the application.
+    """
+    HTTP_BAD_REQUEST = 400
+    HTTP_INTERNAL_SERVER_ERROR = 500
+    HTTP_OK = 200
+    
+    
+    
